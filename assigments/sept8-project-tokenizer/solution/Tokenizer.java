05c96d3319789279339668dd7bae3b7cf639d9cd
Victor Kaiser-Pendergrast <V.Kaiser.Pendergrast@gmail.com>
2014-09-15 19:35:56 -0400

    Added TokenizerProject solution

---
+++ assigments/sept8-project-tokenizer/solution/Tokenizer.java
@@ -0,0 +1,56 @@
+public class Tokenizer {
+
+	// A String of separator characters
+	private String separatorString;
+
+	// A string to be processed into tokens
+	private String tokenString;
+
+	// Where to start looking for tokens when getNextToken() is called
+	private int processIndex = 0;
+
+	public Tokenizer(String sepString, String tokString) {
+		separatorString = sepString;
+		tokenString = tokString;
+	}
+
+	public String getNextToken() {
+		// If processIndex >= tokenString length, there's nothing
+		// left to look for tokens in
+		if(processIndex >= tokenString.length()) {
+			return null;
+		}
+
+		String token = "";
+		for(int i = processIndex; i < tokenString.length(); i++) {
+			if(isSeparatorCharacter(tokenString.charAt(i))) {
+				// End of a token
+				break;
+			} else {
+				// Keep adding to the token
+				token += tokenString.charAt(i);
+			}
+		}
+
+		// Check if the token is blank
+		// (cases where separator characters are adjacent)
+		if(token.isEmpty()) {
+			// We need to try again to find a token
+			return getNextToken();
+		}
+
+		return token;
+	}
+
+	public boolean isSeparatorCharacter(char c) {
+		return (tokenString.indexOf(c) != -1);
+	}
+
+	public void resetToFirstToken() {
+		// All we have to do is reset the processIndex
+		// so that getNextToken() starts from the beginning
+		// of the tokenString 
+		processIndex = 0;
+	}
+
+}
